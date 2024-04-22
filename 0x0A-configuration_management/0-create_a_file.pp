1d88db3e9ee8d23925a55b61d4c7b983cde29b44
techeo <tegaecheo@gmail.com>
2024-04-22 02:39:36 -0700

    Define a file resource

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Define a file resource to create the file '/tmp/school'
+
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
