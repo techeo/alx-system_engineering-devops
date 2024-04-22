d68539b2ae9522e81a8e92554d479d7d7daf18c5
techeo <tegaecheo@gmail.com>
2024-04-22 02:46:27 -0700

    Install flask

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Define a package resource to install Flask via pip3
+package { 'Flask':
+  # Specify the provider as pip3 to install packages using pip3
+  provider => 'pip3',
+  # Ensure that Flask is installed at version 2.1.0
+  ensure   => '2.1.0',
+  # Specify that the package should be installed globally
+  # (You may omit this line if global installation is the default behavior)
+  # install_options => ['--user'],
+}
+
